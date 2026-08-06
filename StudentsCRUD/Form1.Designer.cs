1e54014c24c2cdf6e8afb829f4017bc7f66b5e24
Kyaw Win Soe <kyawwinsoe1718@gmail.com>
2026-08-06 21:54:14 +0630

    Add project files.

---
+++ StudentsCRUD/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace StudentsCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtClass = new System.Windows.Forms.TextBox();
+            this.txtRollNumber = new System.Windows.Forms.TextBox();
+            this.txtDOB = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.studentGripView = new System.Windows.Forms.DataGridView();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnViewDetail = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.studentGripView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Student Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(700, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Addres";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(700, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Class";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(700, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Roll Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(113, 197);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Date Of Birth";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(222, 33);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(253, 33);
+            this.txtId.TabIndex = 6;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(827, 180);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(253, 33);
+            this.txtAddress.TabIndex = 7;
+            // 
+            // txtClass
+            // 
+            this.txtClass.Location = new System.Drawing.Point(827, 98);
+            this.txtClass.Multiline = true;
+            this.txtClass.Name = "txtClass";
+            this.txtClass.Size = new System.Drawing.Size(253, 33);
+            this.txtClass.TabIndex = 8;
+            // 
+            // txtRollNumber
+            // 
+            this.txtRollNumber.Location = new System.Drawing.Point(827, 33);
+            this.txtRollNumber.Multiline = true;
+            this.txtRollNumber.Name = "txtRollNumber";
+            this.txtRollNumber.Size = new System.Drawing.Size(253, 33);
+            this.txtRollNumber.TabIndex = 9;
+            // 
+            // txtDOB
+            // 
+            this.txtDOB.Location = new System.Drawing.Point(222, 180);
+            this.txtDOB.Multiline = true;
+            this.txtDOB.Name = "txtDOB";
+            this.txtDOB.Size = new System.Drawing.Size(253, 33);
+            this.txtDOB.TabIndex = 10;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(222, 98);
+            this.txtName.Multiline = true;
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(253, 33);
+            this.txtName.TabIndex = 11;
+            // 
+            // studentGripView
+            // 
+            this.studentGripView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.studentGripView.Location = new System.Drawing.Point(8, 275);
+            this.studentGripView.Name = "studentGripView";
+            this.studentGripView.RowHeadersWidth = 51;
+            this.studentGripView.RowTemplate.Height = 24;
+            this.studentGripView.Size = new System.Drawing.Size(1413, 254);
+            this.studentGripView.TabIndex = 12;
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnViewAll.Location = new System.Drawing.Point(443, 545);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(534, 51);
+            this.btnViewAll.TabIndex = 13;
+            this.btnViewAll.Text = "View All";
+            this.btnViewAll.UseVisualStyleBackColor = false;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnCreate.Location = new System.Drawing.Point(21, 622);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(173, 44);
+            this.btnCreate.TabIndex = 14;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnDelete.Location = new System.Drawing.Point(757, 622);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(173, 44);
+            this.btnDelete.TabIndex = 15;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnViewDetail
+            // 
+            this.btnViewDetail.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnViewDetail.Location = new System.Drawing.Point(267, 622);
+            this.btnViewDetail.Name = "btnViewDetail";
+            this.btnViewDetail.Size = new System.Drawing.Size(173, 44);
+            this.btnViewDetail.TabIndex = 17;
+            this.btnViewDetail.Text = "View Detail";
+            this.btnViewDetail.UseVisualStyleBackColor = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnClear.Location = new System.Drawing.Point(988, 622);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(173, 44);
+            this.btnClear.TabIndex = 18;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnExit.Location = new System.Drawing.Point(1221, 622);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(173, 44);
+            this.btnExit.TabIndex = 19;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnUpdate.Location = new System.Drawing.Point(518, 622);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(173, 44);
+            this.btnUpdate.TabIndex = 20;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1429, 781);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnViewDetail);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnViewAll);
+            this.Controls.Add(this.studentGripView);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtDOB);
+            this.Controls.Add(this.txtRollNumber);
+            this.Controls.Add(this.txtClass);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Student CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.studentGripView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtClass;
+        private System.Windows.Forms.TextBox txtRollNumber;
+        private System.Windows.Forms.TextBox txtDOB;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DataGridView studentGripView;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnViewDetail;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnUpdate;
+    }
+}
+
---
+++ StudentsCRUD/Form1.cs
@@ -0,0 +1,43 @@
+﻿using StudentsCRUD.DataBaseConnection;
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace StudentsCRUD
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadStudents();
+
+        }
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            LoadStudents();
+           
+        }
+        private void LoadStudents()
+        {
+            using (OleDbConnection conn = DbConnection.GetDbConnection())
+            {
+                conn.Open();
+                string sql = "select * from [students]";
+
+                OleDbDataAdapter adapter = new OleDbDataAdapter(sql, conn);
+
+                DataTable dt = new DataTable();
+
+                adapter.Fill(dt);
+
+                studentGripView.DataSource = dt;
+            }
+        }
+       
+    }
+}
